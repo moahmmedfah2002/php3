06f2bd03191c872b76d4e926818b7572882f8f55
FAHLAOUI MOHAMMED <fahlaouimohammed@gmail.com>
2023-05-12 04:56:59 +0100

    v3

--- module/verificationModule.php
+++ module/verificationModule.php
@@ -2,20 +2,20 @@
 include('../PDO1/migrate.php');
 class verif {
 
-    private $email;
+    private $log;
     private $conn;
     
-    public function __construct($email){
-        $this->email=$email;
+    public function __construct($log){
+        $this->log=$log;
         $this->conn=new PDO1("concours");
         
     }
     public function token(){
-        $token=$this->conn->getData("token","".$_SESSION['niveau']."",'email LIKE "'.$this->email.'"');
+        $token=$this->conn->getData("token","".$_SESSION['niveau']."",'log LIKE "'.$this->log.'"');
         return $token;
     }
     public function valide(){
-        $this->conn->Update($_SESSION['niveau'],"verif","1",'email LIKE "'.$this->email.'"');
+        $this->conn->Update($_SESSION['niveau'],"verif","1",'email LIKE "'.$this->log.'"');
     }
 }
 
